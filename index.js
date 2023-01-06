2e970e4ef42f516c7eb46ba7d44eecde05525f17
LeonelHolguin <Leonelinf16@gmail.com>
2023-01-05 20:31:32 -0400

    added attributes of the component

--- index.js
+++ index.js
@@ -3,11 +3,92 @@ class button extends HTMLElement {
     constructor(){
         super();
 
-        let shadowDOM = this.attachShadow({mode: "open"});
+        this.shadowDOM = this.attachShadow({mode: "open"});
+
 
         this.buttonElement = document.createElement("button");
+        this.buttonElement.style.width = "81px";
+        this.buttonElement.style.height = "36px";
+        
+        
 
-        shadowDOM.appendChild(this.buttonElement);
+        this.shadowDOM.appendChild(this.buttonElement);
+
     }
 
+    connectedCallback() {
+
+        this.buttonElement.innerHTML = this.getAttribute("text");
+
+        this.shadowDOM.innerHTML += `
+        <style>
+            button {
+                width: 100px;
+                height: 100px;
+                color: red; 
+            }
+        </style>`
+        
+        this.setVariant();
+        this.setDisabled();
+
+    }
+
+    setVariant() {
+
+        if(this.hasAttribute("variant") && this.getAttribute("variant") !== "") {
+            
+            console.log("Sirvio")
+
+        }
+
+    }
+
+    setDisableShadow() {
+
+    }
+
+    setDisabled() {
+
+        if(this.hasAttribute("disabled")) {
+
+            console.log("sirvio x2")
+
+            this.buttonElement.createAttribute("disabled");
+
+        }
+
+    }
+
+    setStartIcon() {
+
+    }
+
+    setEndIcon() {
+
+    }
+
+    setSize() {
+
+    }
+
+    setColor() {
+
+    }
+
+    
+
 }
+
+customElements.define("my-button", button);
+
+ /*
+        "text",
+        "variant",
+        "disabledShadow",
+        "disabled",
+        "startIcon",
+        "endIcon",
+        "size",
+        "color"
+        */
